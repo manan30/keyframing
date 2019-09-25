cb6e90283e7bff1fe97dc43a25544b5312554e77
Manan Joshi <mananjoshi1995@gmail.com>
2019-09-25 19:33:15 -0400

    Adding code fab to the main layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Canvas } from './GlobalStyles';
+
+import { Canvas, CodeFAB } from './GlobalStyles';
+import Icon from './svg/ic_code.svg';
 import Scene from './views/Scene';
 
 function App() {
   return (
-    <Canvas>
-      <Scene />
-    </Canvas>
+    <>
+      <Canvas>
+        <Scene />
+      </Canvas>
+      <a
+        href='https://github.com/manan30/keyframing'
+        target='_blank'
+        rel='noopener noreferrer'>
+        <CodeFAB src={Icon} />
+      </a>
+    </>
   );
 }
 
